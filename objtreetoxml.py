1ea09d0f412b73bcd1d362c1416133912fcb047b
aver aver <a.v.e.r@mail.ru>
2021-01-19 11:55:44 +0200

    add сщььутеы

--- objtreetoxml.py
+++ objtreetoxml.py
@@ -61,12 +61,12 @@ class ObjTreeToXML:
         xml_of_this_obj = xml_ET.Element(self.__class__.__name__)
 
         # enumerate and adding properties
-        for prop_name in dir(self.__class__):             # Проходим по именам атрибутов текущего объекта
-            prop = getattr(self.__class__, prop_name)     # получаем этот атрибут
+        for prop_name in dir(self.__class__):             # Проходим по именам атрибутов текущего объекта (из класса)
+            prop = getattr(self.__class__, prop_name)     # получаем этот атрибут (property берется только из класса)
             if isinstance(prop, property):                # проверяем чтобы он был свойством (property)
                 if prop in ObjTreeToXML.__props_for_xml:  # если это свойство в списке для внесения в xml
                     name = prop_name
-                    value = prop.fget(self)
+                    value = prop.fget(self)               # извлекаем значение атрибута объекта
                     print(name, value)
 
         # add data about parent obj
